21d74213ade62a9ece511a6537d78df7b190a9c6
tomnatt <github@tomnatt.com>
2020-11-01 22:28:11 +0000

    Add validations for Game and spec tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  validates :steam_appid, :game_name, :humble_bundle, presence: true
+  validates :steam_appid, uniqueness: true
+  validates :steam_appid, numericality: { only_integer: true }
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have an appid' do
+    game = Game.new(steam_appid: 11, game_name: 'Game 1', humble_bundle: '2020-05-01')
+    expect(game).to be_valid
+
+    game = Game.new(game_name: 'Game 2', humble_bundle: '2020-05-01')
+    expect(game).to_not be_valid
+  end
+
+  it 'must have a unique appid' do
+    game = Game.new(steam_appid: 21, game_name: 'Game 1', humble_bundle: '2020-05-01')
+    game.save!
+
+    game = Game.new(steam_appid: 21, game_name: 'Game 2', humble_bundle: '2020-05-01')
+    expect(game).to_not be_valid
+  end
+
+  it 'must have an appid which is an integer' do
+    game = Game.new(steam_appid: '12aa', game_name: 'Game 1', humble_bundle: '2020-05-01')
+    expect(game).to_not be_valid
+  end
+
+  it 'must have a name' do
+    game = Game.new(steam_appid: 31, game_name: 'Game 1', humble_bundle: '2020-05-01')
+    expect(game).to be_valid
+
+    game = Game.new(steam_appid: 32, humble_bundle: '2020-05-01')
+    expect(game).to_not be_valid
+  end
+
+  it 'must have a date' do
+    game = Game.new(steam_appid: 41, game_name: 'Game 1', humble_bundle: '2020-05-01')
+    expect(game).to be_valid
+
+    game = Game.new(steam_appid: 42, game_name: 'Game 1')
+    expect(game).to_not be_valid
+  end
 end
