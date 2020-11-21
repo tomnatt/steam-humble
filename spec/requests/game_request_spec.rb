689a9b3209bae9c63017c7395a7efe56dfada23e
tomnatt <github@tomnatt.com>
2020-11-21 23:17:59 +0000

    First pass of controller spec

--- spec/requests/game_request_spec.rb
+++ spec/requests/game_request_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Games', type: :request do
+  it 'creates a Game and redirects to the Game page' do
+    get '/games/new'
+    expect(response).to render_template(:new)
+
+    post '/games', params: { game: { steam_appid:   928,
+                                     game_name:     'Controller spec',
+                                     humble_bundle: '2020-05-01' } }
+
+    expect(response).to redirect_to(assigns(:game))
+    follow_redirect!
+
+    expect(response).to render_template(:show)
+    expect(response.body).to include('Game was successfully created.')
+  end
+
+  it 'does not render a different template' do
+    get '/games/new'
+    expect(response).to_not render_template(:show)
+  end
 end
