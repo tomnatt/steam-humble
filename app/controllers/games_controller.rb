5e6e14163afa63af6282e5b2efc92f0b3401b34d
RichWork <richwork1314@gmail.com>
2020-12-13 20:07:28 +0000

    Reverts games_controller.rb and _form.html.erb to 313a346 commit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
-  require "google_drive"
-  require "date"
+
   # GET /games
   # GET /games.json
   def index
@@ -23,12 +22,6 @@ class GamesController < ApplicationController
   # POST /games
   # POST /games.json
   def create
-    steam_id_hash = pull_steam_app_db()
-    humble_games_list = pull_humble_database()
-    final_list = assign_steamid_to_humble_games(steam_id_hash, humble_games_list)
-
-    (0..final_list.size-1).each do |game|
-      @game = game_params
     @game = Game.new(game_params)
 
     respond_to do |format|
@@ -67,100 +60,6 @@ class GamesController < ApplicationController
   end
 
   private
-
-  def pull_humble_database
-
-    session = GoogleDrive::Session.from_service_account_key(ENV['GOOGLE_API_KEY'])
-
-    humble_spreadsheet = "1Y5ySEXPLZdmKFNdMOrGlCEVl6nb_G0X3nYCFSWIdktY"
-    ws = session.spreadsheet_by_key(humble_spreadsheet).worksheets[0]
-
-    srand(777)
-    game_data = []
-
-    (2..ws.num_rows).each do |row|
-      # date of humble bundle
-      date = Date.parse(ws[row, 1]).to_date.to_s
-
-      # early unlock games
-      early_unlock = ws[row, 2].split(', ').map(&:strip)
-
-      if early_unlock.size > 1
-        (0..early_unlock.size-1).each do |game|
-          steam_appid = (rand() * 10000).to_i
-          game = early_unlock[game]
-          game_data.push({date: date, game: game, steam_appid: steam_appid})
-        end
-      else
-        steam_appid = (rand() * 10000).to_i
-        game = early_unlock[0]
-        game_data.push({date: date, game: game, steam_appid: steam_appid})
-      end
-
-      # other games
-      other_games = ws[row, 3].split(', ').map(&:strip)
-
-      if other_games.size > 1
-        (0..other_games.size-1).each do |game|
-          steam_appid = (rand() * 10000).to_i
-          game = other_games[game]
-          game_data.push({date: date, game: game, steam_appid: steam_appid})
-          #all_early_unlocks.push(other_games[game])
-          #p all_early_unlocks
-        end
-      else
-        steam_appid = (rand() * 10000).to_i
-        game = other_games[0]
-        game_data.push({date: date, game: game, steam_appid: steam_appid})
-      end
-
-      # humble originals
-      humble_origs = ws[row, 4].split(', ').map(&:strip)
-
-      if humble_origs.size > 1
-        (0..humble_origs.size-1).each do |game|
-          steam_appid = (rand() * 10000).to_i
-          game = humble_origs[game]
-          game_data.push({date: date, game: game, steam_appid: steam_appid})
-        end
-
-      elsif humble_origs.size == 1 && humble_origs[0] = "N/A" # skips bundles with no originals
-
-      else
-        steam_appid = (rand() * 10000).to_i
-        game = humble_origs[0]
-        game_data.push({date: date, game: game, steam_appid: steam_appid})
-      end
-
-    end
-
-    return game_data
-  end
-
-  def pull_steam_app_db
-    uri = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/'
-    steam_json_file = JSON.parse open(uri).read
-    steam_json_file_raw = steam_json_file['applist']['apps']
-
-    steam_data = {}
-
-    (0..steam_json_file_raw.size).each do |app|
-      if steam_json_file_raw[app].nil?
-      else
-        steam_data[steam_json_file_raw[app]['name']] = steam_json_file_raw[app]['appid']
-      end
-    end
-
-    return steam_data
-  end
-
-  def assign_steamid_to_humble_games(steam_id_hash, humble_games_list)
-    (0..humble_games_list.size-1).each do |model|
-      humble_games_list[model][:steam_appid] = steam_id_hash[humble_games_list[model][:game]]
-    end
-
-    return humble_games_list
-  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_game
